cb3157e7a8503646057b042e75b2a94472a16417
Sergey Kastryulin <snk4tr@gmail.com>
2020-01-14 19:20:09 +0300

    chore(cd): add deployment to pypi cd pipeline

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r") as f, open("requirements.txt", "r") as g:
+    long_description = f.read()
+    required = g.read().splitlines()
+
+
+setuptools.setup(
+    name='photosynthesis_metrics',
+    version='0.0.1',
+    author="Sergey Kastryulin",
+    author_email="snk4tr@gmail.com",
+    description="Measures and metrics for image2image tasks. PyTorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/photosynthesis-team/photosynthesis.metrics",
+    install_requires=required,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
