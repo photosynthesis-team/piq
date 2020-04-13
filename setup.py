93fc863770f20c9aa1d92dd532fb06401764931c
Sergey Kastryulin <snk4tr@gmail.com>
2020-04-13 14:25:28 +0300

    chore(setup): update licence, package release commit

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache-2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
 )
