dea3010cbcf52938b870d91edecd90822ace0cb8
Denis Prokopenko <22414094+denproc@users.noreply.github.com>
2020-05-20 20:25:39 +0300

    enh(utils): Minor changes to improve readability.

--- photosynthesis_metrics/utils.py
+++ photosynthesis_metrics/utils.py
@@ -39,7 +39,7 @@ def _validate_input(
     return
 
 
-def _validate_features(x: torch.Tensor, y: torch.Tensor, ) -> None:
+def _validate_features(x: torch.Tensor, y: torch.Tensor) -> None:
     r"""Check, that computed features satisfy metric requirements.
 
     Args:
@@ -48,11 +48,11 @@ def _validate_features(x: torch.Tensor, y: torch.Tensor, ) -> None:
     """
     assert torch.is_tensor(x) and torch.is_tensor(y), \
         f"Both features should be torch.Tensors, got {type(x)} and {type(y)}"
-    assert len(x.shape) == 2, \
+    assert x.dim() == 2, \
         f"Predicted features must have shape (N_samples, encoder_dim), got {x.shape}"
-    assert len(y.shape) == 2, \
+    assert y.dim() == 2, \
         f"Target features must have shape  (N_samples, encoder_dim), got {y.shape}"
-    assert x.shape[1] == y.shape[1], \
+    assert x.size(1) == y.size(1), \
         f"Features dimensionalities should match, otherwise it won't be possible to correctly compute statistics. \
-            Got {x.shape[1]} and {y.shape[1]}"
+            Got {x.size(1)} and {y.size(1)}"
     assert x.device == y.device, "Both tensors should be on the same device"
