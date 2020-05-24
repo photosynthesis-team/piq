f795dba7d608feccdbca7c353375b0baeacd030d
Mikhail <43113925+smikhai1@users.noreply.github.com>
2020-05-24 23:22:47 +0300

    Bug/issue#57 (#58)
    bug(issue#57): make computation of the TVLoss to be based on only predicted image

--- tests/test_tv.py
+++ tests/test_tv.py
@@ -19,31 +19,24 @@ def test_tv_works(prediction: torch.Tensor) -> None:
     measure = total_variation(prediction, size_average=True, reduction_type='l2')
     assert measure > 0
     measure = total_variation(prediction, size_average=True, reduction_type='l1')
+    assert measure > 0
     measure = total_variation(prediction, size_average=True, reduction_type='l2_squared')
+    assert measure > 0
 
 
-def test_tvloss_init(prediction: torch.Tensor, target: torch.Tensor) -> None:
+def test_tvloss_init() -> None:
     TVLoss()
 
 
-def test_tvloss(prediction: torch.Tensor, target: torch.Tensor) -> None:
+def test_tvloss(prediction: torch.Tensor) -> None:
     loss = TVLoss()
-    res = loss(prediction, target)
+    res = loss(prediction)
     assert res > 0
-
-
-def test_tv_zero_for_equal_tensors(prediction: torch.Tensor):
-    loss = TVLoss()
-    target = prediction.clone()
-    measure = loss(prediction, target)
-    assert measure <= 1e-6, f'TV for equal tensors must be 0, got {measure}'
 
 
 def test_tv_for_known_answer():
     # Tensor with `l1` TV = (10 - 1) * 2  * 2 = 36
     prediction = torch.eye(10).reshape((1, 1, 10, 10))
-    # Tensor with TV = 0
-    target = torch.zeros((1, 1, 10, 10))
     loss = TVLoss(reduction_type='l1')
-    measure = loss(prediction, target)
+    measure = loss(prediction)
     assert measure == 36., f'TV for this tensors must be 36., got {measure}'
