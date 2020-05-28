36c6e81c43f6603e522178052b5e6b0049e64f95
Sergey Kastryulin <snk4tr@gmail.com>
2020-05-28 13:45:35 +0300

    release commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f, open("requirements.txt", "r") as g:
 
 setuptools.setup(
     name='photosynthesis_metrics',
-    version='0.2.0',
+    version='0.3.0',
     author="Sergey Kastryulin",
     author_email="snk4tr@gmail.com",
     description="Measures and metrics for image2image tasks. PyTorch.",
