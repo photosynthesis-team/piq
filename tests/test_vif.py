59bc28e9fc28798e0178c155326f68108f10bc65
Jamil Zakirov <djamilzak@gmail.com>
2020-06-12 13:31:45 +0300

    bug/issue#89: Test to check GPU and result no zero tensors.

--- tests/test_vif.py
+++ tests/test_vif.py
@@ -26,38 +26,47 @@ def target_1d() -> torch.Tensor:
 
 # ================== Test function: `vif_p` ==================
 def test_vif_p_works_for_3_channels(prediction: torch.Tensor, target: torch.Tensor) -> None:
-    try:
-        vif_p(prediction, target, data_range=1.)
-    except Exception as e:
-        pytest.fail(f"Unexpected error occurred: {e}")
+    vif_p(prediction, target, data_range=1.)
 
 
 def test_vif_p_works_for_1_channel(prediction_1d: torch.Tensor, target_1d: torch.Tensor) -> None:
-    try:
-        vif_p(prediction_1d, target_1d, data_range=1.)
-    except Exception as e:
-        pytest.fail(f"Unexpected error occurred: {e}")
+    vif_p(prediction_1d, target_1d, data_range=1.)
+
+
+def test_vif_p_one_for_equal_tensors(prediction: torch.Tensor) -> None:
+    target = prediction.clone()
+    measure = vif_p(prediction, target)
+    print(measure)
+    assert torch.isclose(measure, torch.tensor(1.0)), f'VIF for equal tensors shouls be 1.0, got {measure}.'
+
+
+def test_vif_p_works_for_zeros_tensors() -> None:
+    prediction = torch.zeros(4, 3, 256, 256)
+    target = torch.zeros(4, 3, 256, 256)
+    measure = vif_p(prediction, target, data_range=1.)
+    assert torch.isclose(measure, torch.tensor(1.0)), f'VIF for 2 zero tensors shouls be 1.0, got {measure}.'
 
 
 def test_vif_p_works_for_different_data_range(prediction: torch.Tensor, target: torch.Tensor) -> None:
     prediction_255 = (prediction * 255).type(torch.uint8)
     target_255 = (target * 255).type(torch.uint8)
-    try:
-        vif_p(prediction_255, target_255, data_range=255)
-    except Exception as e:
-        pytest.fail(f"Unexpected error occurred: {e}")
+    vif_p(prediction_255, target_255, data_range=255)
 
 
 # ================== Test class: `VIFLoss` ==================
-def test_vif_loss_init(prediction: torch.Tensor, target: torch.Tensor) -> None:
-    try:
-        VIFLoss()
-    except Exception as e:
-        pytest.fail(f"Unexpected error occurred: {e}")
+def test_vif_loss(prediction: torch.Tensor, target: torch.Tensor) -> None:
+    loss = VIFLoss()
+    loss(prediction, target)
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason='No need to run test if there is no GPU.')
+def test_vif_loss_on_gpu(prediction: torch.Tensor, target: torch.Tensor) -> None:
+    loss = VIFLoss()
+    loss(prediction.cuda(), target.cuda())
 
 
 def test_vif_loss_zero_for_equal_tensors(prediction: torch.Tensor):
     loss = VIFLoss()
     target = prediction.clone()
     measure = loss(prediction, target)
-    assert measure <= 1e-6, f'VIF for equal tensors must be 0, got {measure}'
+    assert torch.isclose(measure, torch.tensor(0.), atol=1e-6), f'VIF for equal tensors must be 0, got {measure}'
