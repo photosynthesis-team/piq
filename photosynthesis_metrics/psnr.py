2ed1993e0ba88d971a4cda0325f5816e8dbdc3df
Jamil Zakirov <djamilzak@gmail.com>
2020-06-16 19:11:18 +0300

    feature/issue#93: Changed reduction method. Added new tests.

--- photosynthesis_metrics/psnr.py
+++ photosynthesis_metrics/psnr.py
@@ -3,53 +3,51 @@ r""" This module implements Peak Signal-to-Noise Ration (PSNR) in PyTorch.
 import torch
 from typing import Optional, Union
 
-from photosynthesis_metrics.utils import _validate_input
+from photosynthesis_metrics.utils import _validate_input, _adjust_dimensions
 
 
-def psnr(prediction: torch.Tensor, target: torch.Tensor,
-         data_range: Union[int, float] = 1.0, reduction: Optional[str] = 'mean'):
+def psnr(x: torch.Tensor, y: torch.Tensor, data_range: Union[int, float] = 1.0,
+         reduction: Optional[str] = 'mean', convert_to_greyscale: bool = False):
     r"""Compute Peak Signal-to-Noise Ration for a batch of images.
     Supports both greyscale and color images with RGB channel order.
 
     Args:
-        prediction: Batch of predicted images with shape (batch_size x channels x H x W)
-        target: Batch of target images with shape  (batch_size x channels x H x W)
+        x: Batch of predicted images with shape (batch_size x channels x H x W)
+        y: Batch of target images with shape  (batch_size x channels x H x W)
         data_range: Value range of input images (usually 1.0 or 255). Default: 1.0
         reduction: Reduction over samples in batch: "mean"|"sum"|"none"
+        convert_to_greyscale: Convert RGB image to YCbCr format and computes PSNR
+            only on luminance channel if `True`. Compute on all 3 channels otherwise.
         
     Returns:
         PSNR: Index of similarity betwen two images.
     Note:
         Implementaition is based on Wikepedia https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
-        Colour images are first converted to YCbCr format and only luminance component is considered.
     """
-    _validate_input((prediction, target), allow_5d=False)
+    _validate_input((x, y), allow_5d=False)
+    x, y = _adjust_dimensions(input_tensors=(x, y))
 
     # Constant for numerical stability
     EPS = 1e-8
 
-    if data_range == 255:
-        prediction = prediction / 255.
-        target = target / 255.
+    x = x / data_range
+    y = y / data_range
 
-    # Convert RGB image to YCbCr and take luminance: Y = 0.299 R + 0.587 G + 0.114 B
-    num_channels = prediction.size(1)
-    if num_channels == 3:
-        prediction = 0.299 * prediction[:, 0, :, :] + 0.587 * prediction[:, 1, :, :] + 0.114 * prediction[:, 2, :, :]
-        target = 0.299 * target[:, 0, :, :] + 0.587 * target[:, 1, :, :] + 0.114 * target[:, 2, :, :]
+    if (x.size(1) == 3) and convert_to_greyscale:
+        # Convert RGB image to YCbCr and take luminance: Y = 0.299 R + 0.587 G + 0.114 B
+        x = 0.299 * x[:, 0, :, :] + 0.587 * x[:, 1, :, :] + 0.114 * x[:, 2, :, :]
+        y = 0.299 * y[:, 0, :, :] + 0.587 * y[:, 1, :, :] + 0.114 * y[:, 2, :, :]
 
         # Add channel dimension
-        prediction = prediction[:, None, :, :]
-        target = target[:, None, :, :]
+        x = x[:, None, :, :]
+        y = y[:, None, :, :]
 
-    mse = torch.mean((prediction - target) ** 2, dim=[1, 2, 3])
-    psnr = - 10 * torch.log10(mse + EPS)
+    mse = torch.mean((x - y) ** 2, dim=[1, 2, 3])
+    score = - 10 * torch.log10(mse + EPS)
 
-    if reduction == 'mean':
-        return psnr.mean(dim=0)
-    elif reduction == 'sum':
-        return psnr.sum(dim=0)
-    elif reduction == 'none':
-        return psnr
-    else:
-        raise ValueError(f'Expected reduction modes are "mean"|"sum"|"none", got {reduction}')
+    if reduction == 'none':
+        return score
+
+    return {'mean': score.mean,
+            'sum': score.sum
+            }[reduction](dim=0)
--- tests/test_psnr.py
+++ tests/test_psnr.py
@@ -7,12 +7,12 @@ from photosynthesis_metrics import psnr
 
 @pytest.fixture(scope='module')
 def prediction() -> torch.Tensor:
-    return torch.rand(4, 3, 256, 256)
+    return torch.rand(4, 3, 128, 128)
 
 
 @pytest.fixture(scope='module')
 def target() -> torch.Tensor:
-    return torch.rand(4, 3, 256, 256)
+    return torch.rand(4, 3, 128, 128)
 
 
 # ================== Test function: `psnr` ==================
@@ -47,6 +47,18 @@ def test_psnr_works_for_zero_tensors() -> None:
     assert torch.isclose(measure, torch.tensor(80.))
 
 
+def test_psnr_works_for_3d_tensors() -> None:
+    prediction = torch.rand(3, 256, 256)
+    target = torch.rand(3, 256, 256)
+    psnr(prediction, target, data_range=1.0)
+
+
+def test_psnr_works_for_2d_tensors() -> None:
+    prediction = torch.rand(256, 256)
+    target = torch.rand(256, 256)
+    psnr(prediction, target, data_range=1.0)
+
+
 def test_psnr_big_for_identical_images(prediction: torch.Tensor) -> None:
     # Max value depends on EPS constant. It's 80 for EPS=1e-8, 100 for EPS=1e-10 etc.
     max_val = torch.tensor(80.)
@@ -67,7 +79,7 @@ def test_psnr_reduction(prediction: torch.Tensor, target: torch.Tensor):
     assert len(measure) == prediction.size(0), \
         f'PSNR with `none` reduction must have length equal to number of images, got {len(measure)}'
 
-    with pytest.raises(ValueError):
+    with pytest.raises(KeyError):
         psnr(prediction, target, reduction='random string')
 
 
@@ -79,3 +91,21 @@ def test_psnr_matches_skimage_greyscale():
 
     assert torch.isclose(pm_measure, torch.tensor(sk_measure, dtype=pm_measure.dtype)), \
         f"Must match Sklearn version. Got: {pm_measure} and skimage: {sk_measure}"
+
+
+def test_psnr_matches_skimage_rgb():
+    prediction = torch.rand(1, 3, 256, 256)
+    target = torch.rand(1, 3, 256, 256)
+    pm_measure = psnr(prediction, target, reduction='mean')
+    sk_measure = peak_signal_noise_ratio(prediction.squeeze().numpy(), target.squeeze().numpy(), data_range=1.0)
+
+    assert torch.isclose(pm_measure, torch.tensor(sk_measure, dtype=pm_measure.dtype)), \
+        f"Must match Sklearn version. Got: {pm_measure} and skimage: {sk_measure}"
+
+
+def test_psnr_loss_backward():
+    prediction = torch.rand(1, 3, 256, 256, requires_grad=True)
+    target = torch.rand(1, 3, 256, 256)
+    loss = 80 - psnr(prediction, target, reduction='mean')
+    loss.backward()
+    assert prediction.grad is not None, 'Expected non None gradient of leaf variable'
