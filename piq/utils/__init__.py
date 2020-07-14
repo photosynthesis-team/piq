09392cded3730200d775999462afece5f2dc209a
Jamil <djamilzak@gmail.com>
2020-07-14 13:02:21 +0500

    Refactor `utils` and `functional` (#135)
    * refactoring/issue#132: Apply structure suggested by @denproc
    
    * refactoring/issue#132: Apply suggestions from code review

---
+++ piq/utils/__init__.py
@@ -0,0 +1,7 @@
+from piq.utils.common import _adjust_dimensions, _validate_input, _validate_features
+
+__all__ = [
+    "_adjust_dimensions",
+    "_validate_input",
+    "_validate_features",
+]
--- piq/vsi.py
+++ piq/vsi.py
@@ -10,7 +10,7 @@ import torch
 from torch.nn.modules.loss import _Loss
 from torch.nn.functional import avg_pool2d, interpolate, pad
 from piq.functional import ifftshift, gradient_map, scharr_filter, rgb2lmn, rgb2lab, similarity_map, get_meshgrid
-from .utils import _validate_input, _adjust_dimensions
+from piq.utils import _validate_input, _adjust_dimensions
 import functools
 import warnings
 
