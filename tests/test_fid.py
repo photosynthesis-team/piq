fbf46915a7f8ce318685492b9dc42f11dfc54410
bes-dev <belbes122@yandex.u>
2020-12-09 14:55:33 +0300

    add test_inception_input_range

--- tests/test_fid.py
+++ tests/test_fid.py
@@ -6,8 +6,9 @@ from piq.feature_extractors import InceptionV3
 
 
 class TestDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        self.data = torch.rand(15, 3, 256, 256)
+    def __init__(self, input_range=(0.0, 1.0)):
+        # self.data = torch.rand(15, 3, 256, 256)
+        self.data = torch.FloatTensor(15, 3, 256, 256).uniform_(*input_range)
         self.mask = torch.rand(15, 3, 256, 256)
 
     def __getitem__(self, index):
@@ -84,5 +85,31 @@ def test_compute_feats_cuda() -> None:
         fid = FID()
         model = InceptionV3()
         fid._compute_feats(loader, model, device='cuda')
+    except Exception as e:
+        pytest.fail(f"Unexpected error occurred: {e}")
+
+
+@pytest.mark.parametrize("input_range,normalize_input,is_correct",
+                         [
+                             ((0.0, 1.0), True, True),
+                             ((-1.0, 1.0), False, True),
+                             ((-1.0, 1.0), True, False),
+                             ((-10.0, 10.0), False, False)
+                         ])
+def test_inception_input_range(input_range, normalize_input, is_correct) -> None:
+    try:
+        dataset = TestDataset(input_range)
+        loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=3,
+            num_workers=2,
+        )
+        fid = FID()
+        model = InceptionV3(normalize_input=normalize_input)
+        if is_correct:
+            fid._compute_feats(loader, model, device='cpu')
+        else:
+            with pytest.raises(Exception):
+                fid._compute_feats(loader, model, device='cpu')
     except Exception as e:
         pytest.fail(f"Unexpected error occurred: {e}")
