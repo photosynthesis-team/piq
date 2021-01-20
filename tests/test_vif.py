98fa0bda001c6fed324538b9d2a5a4c4efd16fc0
zakajd <djamilzak@gmail.com>
2021-01-20 14:17:40 +0300

    bug(vifp): Bump version

--- tests/test_vif.py
+++ tests/test_vif.py
@@ -85,19 +85,19 @@ def test_vif_simmular_to_matlab_implementation():
     score = vif_p(goldhill_jpeg, goldhill, data_range=255, reduction='none')
     score_baseline = torch.tensor(0.2665)
 
-    assert torch.isclose(score, score_baseline), \
+    assert torch.isclose(score, score_baseline, atol=1e-4), \
         f'Expected PyTorch score to be equal to MATLAB prediction. Got {score} and {score_baseline}'
 
     # RGB images
     I01 = torch.tensor(imread('tests/assets/I01.BMP')).permute(2, 0, 1)
     i1_01_5 = torch.tensor(imread('tests/assets/i01_01_5.bmp')).permute(2, 0, 1)
 
-    score = vif_p(i1_01_5, I01, data_range=255, chromatic=False, reduction='none')
+    score = vif_p(i1_01_5, I01, data_range=255, reduction='none')
 
     # RGB images are not supported by MATLAB code. Here is result for luminance channel taken from YIQ colour space
     score_baseline = torch.tensor(0.3147)
 
-    assert torch.isclose(score, score_baseline), \
+    assert torch.isclose(score, score_baseline, atol=1e-4), \
         f'Expected PyTorch score to be equal to MATLAB prediction. Got {score} and {score_baseline}'
 
 
