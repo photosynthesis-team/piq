27c3b76c47e3e1341c891396e7d94c7e000a0ba5
Cartoon <61895686+CartoonTa@users.noreply.github.com>
2021-02-04 22:38:35 +0700

    initial commit

--- piq/tv.py
+++ piq/tv.py
@@ -9,7 +9,27 @@ from piq.utils import _validate_input, _adjust_dimensions
 
 def total_variation(x: torch.Tensor, reduction: str = 'mean', norm_type: str = 'l2') -> torch.Tensor:
     r"""Compute Total Variation metric
+    ## 1D signal series 
+    For a digital signal $y_{n}$, we can, for example, define the total variation as
+    $$
+    V(y)=\sum _{n}|y_{n+1}-y_{n}|.
+    $$
+    Given an input signal $x_{n}$, the goal of total variation denoising is to find an approximation, call it $y_{n}$, that has smaller total variation than $x_{n}$ but is "close" to $x_{n}$. One measure of closeness is the sum of square errors:
+    $$
+    \mathrm{E}(x, y)=\frac{1}{n} \sum_{n}\left(x_{n}-y_{n}\right)^{2}.
+    $$
 
+    So the total-variation denoising problem amounts to minimizing the following discrete functional over the signal $y_{n}$:
+    $$
+    \mathrm {E} (x,y)+\lambda V(y).
+    $$
+
+    By differentiating this functional with respect to $y_{n}$, we can derive a corresponding Euler–Lagrange equation, that can be numerically integrated with the original signal $x_{n}$ as initial condition. This was the original approach. Alternatively, since this is a convex functional, techniques from convex optimization can be used to minimize it and find the solution $y_{n}$.
+
+    ## Regularization properties
+
+    The regularization parameter $\lambda$ plays a critical role in the denoising process. When $\lambda =0$, there is no smoothing and the result is the same as minimizing the sum of squares. As $\lambda \to \infty $, however, the total variation term plays an increasingly strong role, which forces the result to have smaller total variation, at the expense of being less like the input (noisy) signal. Thus, the choice of regularization parameter is critical to achieving just the right amount of noise removal.
+    
     Args:
         x: Tensor with shape (N, C, H, W).
         reduction: Reduction over samples in batch: "mean"|"sum"|"none"
