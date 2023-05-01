86f02dac0f655889015c3b58db2b3b677a269471
Sergey Kastryulin <snk4tr@gmail.com>
2023-05-01 22:18:34 +0300

    remove ftfy package

--- piq/tokenizers/clip.py
+++ piq/tokenizers/clip.py
@@ -1,5 +1,4 @@
 import torch
-import ftfy
 import html
 import os
 import gzip
@@ -190,7 +189,6 @@ def get_pairs(word):
 
 
 def basic_clean(text):
-    text = ftfy.fix_text(text)
     text = html.unescape(html.unescape(text))
     return text.strip()
 
